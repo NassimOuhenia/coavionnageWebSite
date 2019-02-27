e1be4d67bb549928ad9b2076dd379a8a2cbfc727
lynda <medjkoun@informatique.univ-paris-diderot.fr>
2019-02-27 00:49:25 +0000

    objets metier et interfeces DAO

---
+++ td4classDAO/src/classDao/Pilot.java
@@ -0,0 +1,29 @@
+package classDao;
+
+public class Pilot {
+
+	private String mail;
+	private double experiences;
+
+	public Pilot(String m) {
+		mail = m;
+		experiences = 0;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String m) {
+		mail = m;
+	}
+
+	public double getExperiences() {
+		return experiences;
+	}
+
+	public void setExperiences(String m) {
+		mail = m;
+	}
+
+}
