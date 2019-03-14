fde1af9f82f69fc2467af89f0135112231d06865
nas <nassim.ouhenia@yahoo.com>
2019-03-14 16:56:56 +0100

    methode get pour la recherche des vols

--- BouchonWS/src/main/java/com/example/jetty_jersey/ws/FlightRessource.java
+++ BouchonWS/src/main/java/com/example/jetty_jersey/ws/FlightRessource.java
@@ -1,11 +1,11 @@
 package com.example.jetty_jersey.ws;
 
-
 import java.util.List;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -17,33 +17,28 @@ import com.example.jetty_jersey.model.Flight;
 import com.example.jetty_jersey.model.Pilot;
 import com.example.jetty_jersey.model.Plane;
 
-
 @Path("/flights")
 public class FlightRessource {
 
 	private FlightDAO daoFlight = DAOFactory.getInstance().getFlightDAO();
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/flight/")
-	public Flight getExample(@PathParam("id") List<String> id) {
-		
-		System.out.println(id);
-		//just test
-		return daoFlight.get().get(0);
-		
+	@Path("/flight")
+	public List<Flight> getExample(@QueryParam("departure") String departure, @QueryParam("arrival") String arrival,
+			@QueryParam("date") String date, @QueryParam("travel") String travel, @QueryParam("local") String local) {
+
+		// just test
+		return daoFlight.search(date, departure, arrival, local, travel);
+
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/flight")
-	public List<Flight> retrieveExample(@FormParam("departure") String departure,@FormParam("arrival") String arrival
-			,@FormParam("date") String date,@FormParam("travel") String travel,@FormParam("local") String local) {
-		
-		return daoFlight.search(date, departure, arrival, local, travel);
+	public List<Flight> retrieveExample() {
+		return null;
 	}
 
-	
-	
 }
