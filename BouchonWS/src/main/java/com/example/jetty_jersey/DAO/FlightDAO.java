1a44bb9502108a3d841da49d7aed8598cdb7ec3d
NJANGUI wilfried <njangui@informatique.univ-paris-diderot.fr>
2019-03-20 12:14:49 +0100

    Passengers  + Flight terminee

--- BouchonWS/src/main/java/com/example/jetty_jersey/DAO/FlightDAO.java
+++ BouchonWS/src/main/java/com/example/jetty_jersey/DAO/FlightDAO.java
@@ -1,28 +1,52 @@
 package com.example.jetty_jersey.DAO;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
 
 import com.example.jetty_jersey.model.*;
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
 
 public class FlightDAO extends DAO<Flight> {
 	private static List<Flight> list=new ArrayList<Flight>();
 	//un fois la bdd est en place
-	//private DAOFactory daofactory;
+	private DAOFactory daofactory;
 
 	public FlightDAO(DAOFactory f) {
 		list.add(new Flight("1","13/07/2019", "orly", "CDG", 35, 2, "travel", new Plane(), new Pilot()));
 		list.add(new Flight("2","13/07/2019", "orly", "CDG", 35, 2, "travel", new Plane(), new Pilot()));
 		list.add(new Flight("3","13/07/2019", "paris", "paris", 35, 2, "travel", new Plane(), new Pilot()));
 		
-		//daofactory = f;
+		daofactory = f;
 	}
+	
 
 	@Override
 	public IndexResponse put(Flight obj) {
-
+		TransportClient  client = daofactory.getConnextion(); 
+		try { 
+			IndexResponse response = client.prepareIndex("flight","_doc").setSource(
+					jsonBuilder()
+					.startObject()
+					.field("idFlight",obj.getIdFlight())
+					.field("date",obj.getDate())
+					.field("departureAirport",obj.getDeparture_airport())
+					.field("arrivalAirport",obj.getArrival_airport())
+					.field("travelTime",obj.getTravelTime())
+					.field("price",obj.getPrice())
+					.field("typeflight",obj.getTypeflight())
+					.field("plane",obj.getPlane())
+					.field("pilot",obj.getPilot())
+					.field("passagers",obj.getPassagers())
+					.endObject()
+					).get(); 
+			return response; 
+		}catch (IOException e ) { 
+			e.printStackTrace();
+		}
 		return null;
 	}
 
--- BouchonWS/src/main/java/com/example/jetty_jersey/DAO/PassengerDAO.java
+++ BouchonWS/src/main/java/com/example/jetty_jersey/DAO/PassengerDAO.java
@@ -1,9 +1,14 @@
 package com.example.jetty_jersey.DAO;
 
+import java.io.IOException; 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.rest.RestStatus;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
 
 import com.example.jetty_jersey.model.Passenger;
 
@@ -11,15 +16,32 @@ import com.example.jetty_jersey.model.Passenger;
 public class PassengerDAO extends DAO<Passenger>{
 	private static List<Passenger> list=new ArrayList<Passenger>();
 	//un fois la bdd est en place
-	//private DAOFactory daofactory;
+	private DAOFactory daofactory;
 
 	public PassengerDAO(DAOFactory f) {
 
-		//daofactory = f;
+		daofactory = f;
 	}
 
 	@Override
 	public IndexResponse put(Passenger obj) {
+		TransportClient client = daofactory.getConnextion();  
+		try { 
+			IndexResponse response = client.prepareIndex("passager", "_doc").setSource(
+					jsonBuilder()
+					.startObject()
+					.field("firstName",obj.getFirstName())
+					.field("lastName",obj.getLastName())
+					.field("mail",obj.getMail())
+					.field("password",obj.getPassword())
+					.endObject()
+					).get(); 
+			return response; 
+		}
+		catch (IOException e) { 
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
