e2f60548a328850603b2ec74e906f677802c7db7
Jonathan <rabemananjarajonathan@yahoo.com>
2019-03-21 16:22:31 +0100

    flight modif

---
+++ BouchonWS/src/main/java/com/example/jetty_jersey/model/Reservation.java
@@ -0,0 +1,44 @@
+package com.example.jetty_jersey.model;
+
+public class Reservation {
+	private String idPassenger;
+	private String idFlight;
+	private int numberPlace;
+
+	public Reservation() {
+
+	}
+
+	public Reservation(String idPassenger, String idFlight, int numberPlace) {
+		super();
+		this.idPassenger = idPassenger;
+		this.idFlight = idFlight;
+		this.numberPlace = numberPlace;
+	}
+
+	public String getIdPassenger() {
+		return idPassenger;
+	}
+
+	public void setIdPassenger(String idPassenger) {
+		this.idPassenger = idPassenger;
+	}
+
+	public String getIdFlight() {
+		return idFlight;
+	}
+
+	public void setIdFlight(String idFlight) {
+		this.idFlight = idFlight;
+	}
+
+	public int getNumberPlace() {
+		return numberPlace;
+	}
+
+	public void setNumberPlace(int numberPlace) {
+		this.numberPlace = numberPlace;
+	}
+	
+	
+}
