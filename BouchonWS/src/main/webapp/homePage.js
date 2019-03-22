4e881bc1abd25a6033a539af55cf63d38a96b275
Jonathan <rabemananjarajonathan@yahoo.com>
2019-03-22 10:06:33 +0100

    planifié vol ( a ameliorer )

--- BouchonWS/src/main/webapp/homePage.js
+++ BouchonWS/src/main/webapp/homePage.js
@@ -1,45 +1,67 @@
+var urlSearchFlight='http://localhost:8080/blablaplane/flights/search';
+var urlPostFlight='http://localhost:8080/blablaplane/flights/add';
 
-	function myFunction(listF) {
-		$("#resultr").text("");
-		//alert("je parse");
-		/*var obj=JSON.parse(listF);
-		console.log("je parse "+listF.length);*/
-		console.log(listF.length);
-		for(i=0;i<listF.length;i++){
-			var templateExample = _.template($('#ajoutr').html());
-			var html = templateExample({
-				"depart":listF[i].departureAirport,
-				"arrive":listF[i].arrivalAirport,
-				"date":listF[i].date,
-				"type":listF[i].typeflight
-			});
-			$("#resultr").append(html);
-			console.log(listF[i].departureAirport+" "+listF[i].arrivalAirport+" "+listF[i].price+" "+listF[i].travelTime);
-			//console.log(html);
-		}
-	}
-	
-$(function(){
-	$("#rvol").click(function(){
-		var donne={typeLocal:$("#local").val(),
-			typeTravel:$("#travel").val(),
-			date:$("#date").val(),
-			departure:$("#departure").val(),
-			arrival:$("#arrival").val()
-		};
-		$.ajax({
-			url: 'http://localhost:8080/blablaplane/flights/search',
-			type: 'post',
-			dataType: 'json',
-			contentType: 'application/json',
-			success: function (data) {
-				myFunction(data);
-				//alert("ok");
-			},
-			data: JSON.stringify(donne),
-			error : function(jqXHR, textStatus, errorThrown) {
-				alert("pas ok");
-			}
+function afterSearch(listF) {
+	$("#resultr").text("");
+	console.log(listF.length);
+	for(i=0;i<listF.length;i++){
+		var templateExample = _.template($('#ajoutr').html());
+		var html = templateExample({
+			"depart":listF[i].departureAirport,
+			"arrive":listF[i].arrivalAirport,
+			"date":listF[i].date,
+			"type":listF[i].typeflight
 		});
+		$("#resultr").append(html);
+		//console.log(listF[i].departureAirport+" "+listF[i].arrivalAirport+" "+listF[i].price+" "+listF[i].travelTime);
+	}
+}
+
+//fonction generique pour recuperer des donnes
+function getServerData(url, callBack, type, data){
+    $.ajax({
+		url: url,
+		type: type,
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function (data) {
+			if(callBack)
+				callBack(data);
+		},
+		data: JSON.stringify(data),
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert("pas ok");
+		}
+	});
+}
+
+//click de bouton recherche vol
+$(function(){
+	$("#rFlight").click(function(){
+		//construction des donne de recherche
+		var data={typeLocal:$("#localr").val(),
+			typeTravel:$("#travelr").val(),
+			date:$("#dater").val(),
+			departure:$("#departurer").val(),
+			arrival:$("#arrivalr").val()
+		};
+		getServerData(urlSearchFlight, afterSearch, 'post', data);
+	});
+});
+
+//click de bouton planifié vol
+$(function(){
+	$("#pFlight").click(function(){
+		//construction des donne du vol
+		var data={
+			date:$("#datep").val(),
+			departureAirport:$("#departurep").val(),
+			arrivalAirport:$("#arrivalp").val(),
+			travelTime:$("#travelTimep").val(),
+			price:$("#pricep").val(),
+			typeflight:"local"
+		};
+		alert(data);
+		getServerData(urlPostFlight,null, 'post', data);
 	});
 });
